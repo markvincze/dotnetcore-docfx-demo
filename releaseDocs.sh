2083f195ce310c242a90b72d6ee3e74c1d4813d2
markvincze <mrk.vincze@gmail.com>
2017-11-16 23:37:23 +0100

    Configure GitHub authentication

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -14,7 +14,7 @@ rm -rf $TEMP_REPO_DIR
 mkdir $TEMP_REPO_DIR
 
 echo "Cloning the repo with the gh-pages branch"
-git clone git@github.com:markvincze/dotnetcore-docfx-demo.git --branch gh-pages $TEMP_REPO_DIR
+git clone https://github.com/markvincze/dotnetcore-docfx-demo.git --branch gh-pages $TEMP_REPO_DIR
 
 echo "Clear repo directory"
 cd $TEMP_REPO_DIR
